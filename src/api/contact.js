8f6d0ecf5b8550e3de1b1cadc001defd9a98b133
David Uebelacker <david.endress@endress.com>
2022-01-08 15:52:18 +0100

    :art: fixed contact mailing

--- src/api/contact.js
+++ src/api/contact.js
@@ -1,9 +1,23 @@
-import mailgun from 'mailgun.js';
+import Mailgun from 'mailgun.js';
+import FormData from 'form-data';
+import axios from 'axios';
 import * as Yup from 'yup';
+
+const mailgun = new Mailgun(FormData);
 
 Yup.addMethod(Yup.string, 'captcha', function captcha() {
   return this.test('captcha', 'Captcha validation failed', (value) => new Promise((resolve, reject) => {
-    setTimeout(() => resolve(false), 5000);
+    const data = new FormData();
+    data.append('secret', process.env.RECAPTCHA_SECRET);
+    data.append('response', value);
+    axios.post('https://www.google.com/recaptcha/api/siteverify', data, {
+      headers: data.getHeaders(),
+    }).then((response) => {
+      resolve(response.data.success);
+    }).catch((error) => {
+      console.error(error);
+      reject(error);
+    });
   }));
 });
 
@@ -22,39 +36,21 @@ export default {
 
     try {
       await messageSchema.validate(message);
+      await mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY })
+        .messages.create('mg.codecowboys.io', {
+          from: message.email,
+          to: ['support@codecowboys.io'],
+          subject: message.subject,
+          text: message.message,
+        });
       response.status(204).send();
     } catch (error) {
       console.error(error);
-      response.status(400).send(JSON.stringify({ error: 'validation failed', causes: error.errors }));
+      if (error.errors) {
+        response.status(400).send(JSON.stringify({ error: 'validation failed', causes: error.errors }));
+      } else {
+        response.status(400).send(JSON.stringify({ error: 'unknown error' }));
+      }
     }
-
-    // if (!isEmpty(message.email)
-    // && validator.isEmail(message.email)
-    // && !isEmpty(message.subject)
-    // && !isEmpty(message.message)
-    // && message.privacy) {
-    //   recaptcha.checkResponse(message.captcha, (recaptchaError, recaptchaResponse) => {
-    //     if (recaptchaError || !recaptchaResponse.success) {
-    //       console.log('Captcha invalid :-(');
-    //       console.error(recaptchaError);
-    //       response.status(400).send();
-    //     } else {
-    //       console.log('Captcha valid ... will send');
-    //       mailgun.client({ username: 'api', key: process.env.MAILGUN_API_KEY }).messages.create('mg.codecowboys.io', {
-    //         from: message.email,
-    //         to: ['support@codecowboys.io'],
-    //         subject: message.subject,
-    //         text: message.message,
-    //       }).then(() => response.status(204).send())
-    //         .catch((error) => {
-    //           console.log('Mailgun error :-(');
-    //           console.error(error);
-    //           response.status(400).send();
-    //         });
-    //     }
-    //   });
-    // } else {
-    //   response.status(400).send();
-    // }
   },
 };
