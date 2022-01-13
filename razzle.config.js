5a46fb06aa265ec1419f13189525617d061f7d71
David Uebelacker <david.endress@endress.com>
2022-01-13 22:26:05 +0100

    :ok_hand: fixing build

--- razzle.config.js
+++ razzle.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   plugins: ['scss', 'mdx'],
+
+  modifyWebpackConfig(opts) {
+    const config = { ...opts.webpackConfig };
+
+    config.performance = {
+      maxAssetSize: 1000,
+      maxEntrypointSize: 300000,
+      hints: false,
+    };
+
+    return config;
+  },
 };
